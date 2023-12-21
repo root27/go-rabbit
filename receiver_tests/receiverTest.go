5731764111f75c7e09059f7ddb5bc432d472b6b9
root27 <oguzhaneee@gmail.com>
2023-12-21 17:30:53 +0300

    updated

--- receiver_tests/receiverTest.go
+++ receiver_tests/receiverTest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	amqplib "github.com/root27/go-rabbit"
@@ -20,6 +21,19 @@ func main() {
 
 	// Receive a message
 
-	amqplib.Receive(conn, "test2")
+	amqplib.Receive(conn, "test2", ConverttoJson)
+
+}
+
+var test map[string]interface{}
+
+func ConverttoJson(body []byte) {
+	err := json.Unmarshal(body, &test)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("message converted: %s", test)
 
 }
